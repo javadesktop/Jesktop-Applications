ecf143dc75395c603237cbf64cae30dfe55b1240
paul-h <paul-h@94f5763d-0f86-49e2-a961-75b90a28ddd6>
2004-03-04 23:28:33 +0000

    this helps

---
+++ build.bat
@@ -0,0 +1,67 @@
+cd Beanshell
+start build dist
+cd ..
+
+cd Clock
+start build dist
+cd ..
+
+cd Demos
+start build dist
+cd ..
+
+cd Eternity
+start build dist
+cd ..
+
+cd HorstScape2
+start build dist
+cd ..
+
+cd JCDSee
+start build dist
+cd ..
+
+cd JCalculator
+start build dist
+cd ..
+
+cd JFileManager
+start build dist
+cd ..
+
+cd JPad
+start build dist
+cd ..
+
+cd JTetris
+start build dist
+cd ..
+
+cd JarUtil
+start build dist
+cd ..
+
+cd Jipe
+start build dist
+cd ..
+
+cd Oyoaha
+start build dist
+cd ..
+
+cd SkinLF
+start build dist
+cd ..
+
+cd Textpad
+start build dist
+cd ..
+
+cd XBrowser
+start build dist
+cd ..
+
+cd DotNetWebsite
+start build dist
+cd ..
